da40bda023e2de872e05f939f933edf8487f1a8d
mohammad-ash <125576618+mohammad-ash@users.noreply.github.com>
2025-03-02 15:48:33 +0200

    First task

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    $name = 'Mohammed';
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+    // return view('about')->with('name', $name);
+    // return view('about', ['name' => $name]);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '01' => 'Tichnical',
+        '02' => 'Financial',
+        '03' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
